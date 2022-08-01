8badf09dc5e80649058257ee561a0e62fa9808bd
larritola1 <lorenzoarritola@gmail.com>
2022-08-01 04:47:34 -0400

    Create multiply function

--- calc-js.js
+++ calc-js.js
@@ -5,3 +5,7 @@ function add(addend1, addend2) {
 function subtract(operand1, operand2) {
     return operand1 - operand2;
 }
+
+function multiply(operand1, operand2) {
+    return operand1 * operand2;
+}

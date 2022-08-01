39e34494617d66d1697a7fd7375edc0d2e8bb6d6
larritola1 <lorenzoarritola@gmail.com>
2022-08-01 04:59:10 -0400

    Create operate function

--- calc-js.js
+++ calc-js.js
@@ -13,3 +13,17 @@ function multiply(operand1, operand2) {
 function divide(operand1, operand2) {
     return operand1 / operand2;
 }
+
+function operate(operand1, operand2, operator) {
+    switch (operator) {
+        case "+":
+            return add(operand1, operand2);
+        case "-":
+            return subtract(operand1, operand2);
+        case "*":
+            return multiply(operand1, operand2);
+        case "/":
+            return divide(operand1, operand2);
+    }
+}
+

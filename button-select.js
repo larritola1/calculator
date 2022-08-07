a41644816d79b3b90848ef59444328204ee14a21
larritola1 <lorenzoarritola@gmail.com>
2022-08-07 05:29:38 -0400

    Deselect any currently highlighted buttons

--- button-select.js
+++ button-select.js
@@ -1,7 +1,17 @@
 inputs.forEach((button) => {
     button.addEventListener("click", () => {
+        deselectButton();
+        
         if (button.className !== "active") {
             button.classList.add("active");
         }
     });
 });
+
+function deselectButton() {
+    inputs.forEach((button) => {
+        if (button.className == "active") {
+            button.classList.remove("active");
+        }
+    });
+}
